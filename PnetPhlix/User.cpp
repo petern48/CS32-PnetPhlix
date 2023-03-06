5f3b5be1a7c0b2ce550ecfbbcb0222728c79cb5f
Peter Nguyen <petern0408@gmail.com>
2023-03-06 15:59:54 -0800

    Set up Movie and User

--- PnetPhlix/User.cpp
+++ PnetPhlix/User.cpp
@@ -8,19 +8,20 @@ User::User(const string& full_name, const string& email,
            const vector<string>& watch_history)
 {
     // Replace this line with correct code.
+    m_name = full_name;
+    m_email = email;
+    m_watchHistory = watch_history;
 }
 
-string User::get_full_name() const
-{
-    return "DUMMY";  // Replace this line with correct code.
+
+string User::get_full_name() const {
+    return m_name;
 }
 
-string User::get_email() const
-{
-    return "DUMMY";  // Replace this line with correct code.
+string User::get_email() const {
+    return m_email;
 }
 
-vector<string> User::get_watch_history() const
-{
-    return vector<string>();  // Replace this line with correct code.
+vector<string> User::get_watch_history() const {
+    return m_watchHistory;
 }
