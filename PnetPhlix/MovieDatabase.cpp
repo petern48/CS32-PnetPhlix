46abe8877517b7c5d2fa7dd0e43c971514093368
Peter Nguyen <petern0408@gmail.com>
2023-03-09 13:25:10 -0800

    Save

--- PnetPhlix/MovieDatabase.cpp
+++ PnetPhlix/MovieDatabase.cpp
@@ -8,10 +8,16 @@ using namespace std;
 // I added the following
 #include <iostream>
 #include <fstream>
+#include <sstream> // To convert string to int
 
 MovieDatabase::MovieDatabase()
 {
-    // Replace this line with correct code.
+    m_tree = new TreeMultimap<string, vector<Movie*>>;
+}
+
+MovieDatabase::~MovieDatabase() {
+    if (m_tree != nullptr)
+        delete m_tree;
 }
 
 bool MovieDatabase::load(const string& filename)
@@ -21,30 +27,45 @@ bool MovieDatabase::load(const string& filename)
         cerr << "Error: Couldn't open file " << filename << endl;
         return false;
     }
+    while (true) {
+        string currLine, temp;
+        string id;
+        string title;
+        string releaseYear;
+        vector<string> directors; // Maybe just insert as a string with commas
+        vector<string> actors;
+        vector<string> genres;
+        int rating, numDirectors, numActors, numGenres;
 
-    string currLine;
-    string id;
-    string title;
-    string releaseYear;
-    vector<string> directors; // Maybe just insert as a string with commas
-    vector<string> actors;
-    vector<string> genres;
-    int rating, numDirectors, numActors, numGenres;
+        int lineCount = 1;
 
-    int lineCount = 1;
-
-    while (getline(infile, currLine)) {
-        if (lineCount == IDLINENUMBER)
-            id = currLine;
-        else if (lineCount == TITLELINENUMBER)
-            title = currLine;
-        else if (lineCount == YEARLINENUMBER)
-            releaseYear = currLine;
-        // CONT
+        // Read in the first 3 lines
+        while (getline(infile, currLine)) {
+            if (lineCount == IDLINENUMBER)
+                id = currLine;
+            else if (lineCount == TITLELINENUMBER)
+                title = currLine;
+            else if (lineCount == YEARLINENUMBER) {
+                releaseYear = currLine;
+                break;
+            }
+            lineCount++;
+        }
+        parseLine(infile, directors);
+        parseLine(infile, actors);
+        parseLine(infile, genres);
+        stringstream ss;
+        if (getline(infile, currLine)) {
+            ss << currLine;
+            ss >> rating;
+        }
+        
+        // We should now be at the end of the Movie Record
+        if (!getline(infile, currLine))
+            break;
     }
 
-
-    return false;  // Replace this line with correct code.
+    return true;  // Replace this line with correct code.
 }
 
 Movie* MovieDatabase::get_movie_from_id(const string& id) const
@@ -65,4 +86,20 @@ vector<Movie*> MovieDatabase::get_movies_with_actor(const string& actor) const
 vector<Movie*> MovieDatabase::get_movies_with_genre(const string& genre) const
 {
     return vector<Movie*>();  // Replace this line with correct code.
+}
+
+void parseLine(ifstream& infile, std::vector<std::string>& v) {
+    string currLine;
+    // Make into function
+    // Read each word until no comma encountered
+    while (infile >> currLine) {
+        // Remove the comma if there is one
+        if (currLine.back() == ',')
+            currLine.pop_back();
+        // If no comma, we reached end of line
+        else
+            return;
+
+        v.push_back(currLine); // Push words to vector
+    }
 }
--- PnetPhlix/MovieDatabase.h
+++ PnetPhlix/MovieDatabase.h
@@ -4,9 +4,15 @@
 #include <string>
 #include <vector>
 
+#include "treemm.h"
+
 const int IDLINENUMBER = 1;
 const int TITLELINENUMBER = 2;
 const int YEARLINENUMBER = 3;
+const int DIRECTORLINENUMBER = 4;
+const int ACTORLINENUMBER = 5;
+const int GENRELINENUMBER = 6;
+const int RATINGLINENUMBER = 7;
 
 class Movie;
 
@@ -14,6 +20,7 @@ class MovieDatabase // CASE INSENSITIVE search
 {
   public:
     MovieDatabase();
+    ~MovieDatabase(); // Added Destructor
     bool load(const std::string& filename);
     Movie* get_movie_from_id(const std::string& id) const;
     std::vector<Movie*> get_movies_with_director(const std::string& director) const;
@@ -21,6 +28,9 @@ class MovieDatabase // CASE INSENSITIVE search
     std::vector<Movie*> get_movies_with_genre(const std::string& genre) const;
 
   private:
+      TreeMultimap <std::string, std::vector<Movie*>> *m_tree;
+        
+      void parseLine(ifstream& infile, std::vector<std::string>& v);
 };
 
 #endif // MOVIEDATABASE_INCLUDED
